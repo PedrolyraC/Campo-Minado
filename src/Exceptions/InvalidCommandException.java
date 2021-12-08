a4bc371d75ff5ba64b41d30220a0436084bb0a5e
PedrolyraC <60264595+PedrolyraC@users.noreply.github.com>
2021-12-08 13:18:15 -0300

    Delete src directory

--- src/Exceptions/InvalidCommandException.java
+++
@@ -1,5 +0,0 @@
-package Exceptions;
-
-public class InvalidCommandException extends RuntimeException {
-
-}
--- src/Exceptions/TryToChooseFlagSquareException.java
+++
@@ -1,5 +0,0 @@
-package Exceptions;
-
-public class TryToChooseFlagSquareException extends RuntimeException{
-
-}
